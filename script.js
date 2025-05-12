f8a5dedd5822df9b34e3ec6dd67c7fb2f9760855
tusharkuma <tusharkumar121001@gmail.com>
2025-05-12 18:01:10 +0530

    final edits

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // or when user is very close to bottom of page
         if (
           // When contact section is at end or almost finished
-          contactRect.bottom <= window.innerHeight * 0.9 ||
+          contactRect.top <= window.innerHeight * 0.9 ||
           // When almost at bottom of page
           (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 200
         ) {
